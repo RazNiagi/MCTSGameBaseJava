ea714a1b02c55767de8f73a5af236f17e6e38ce2
RazNiagi <RazNiagi@gmail.com>
2024-11-13 20:35:55 -0500

    Change from generic import to specific

--- src/main/java/com/example/mctsbase/model/OnitamaMovementCard.java
+++ src/main/java/com/example/mctsbase/model/OnitamaMovementCard.java
@@ -1,7 +1,10 @@
 package com.example.mctsbase.model;
 
 import com.example.mctsbase.enums.OnitamaMovementBias;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 
